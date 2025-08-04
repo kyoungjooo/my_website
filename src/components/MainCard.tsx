be21f386f37c674f35d19a71bf686d52e4f80411
kyoungjooo <leekjoo1008@gmail.com>
2025-08-04 16:38:21 +0900

    feat: 이미지 추가

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./common/Button";
 import Card from "./common/Card";
+import project_02 from "../assets/images/project_02.png";
 
 export default function MainCard() {
   const navigate = useNavigate();
   const handleGoToProjectList = () => {
     navigate("/project");
   };
+
+  const PROJECTS = [{ name: "스마트보장분석", image: project_02 }];
   return (
     <Card>
       <div className="main-card">
@@ -17,9 +20,15 @@ export default function MainCard() {
               <span className="sr-only">더보기</span>
             </button>
           </header>
-          <div className="main-card__content" onClick={handleGoToProjectList} />
+          <div
+            className="main-card__content flex-center"
+            onClick={handleGoToProjectList}
+          >
+            <img src={PROJECTS[0].image} alt="" className="h-full" />
+          </div>
           <footer className="main-card__footer">
-            <Button text="see all work" />
+            <p className="px-12">{PROJECTS[0].name}</p>
+            <Button text="see all work" onClick={handleGoToProjectList} />
           </footer>
         </div>
       </div>
